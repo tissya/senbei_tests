9788f53b23921396cd8d8f541ee1f4653d070643
nameko1 <nameko816.07@gmail.com>
2014-11-09 02:08:19 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,5 @@
-public senbei_test{
-  かな子はかわいいなあ
-  }
+これを1行コピーして登録してちょ
+わからんかったらgit ssh 公開鍵　とかで検索したらなにかわかるかも
+
+
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCZe3dWipFQia2OnUYC4uhBVOvIGn4YlIWRttNYnuG/nXXA2lYP1ZkGiuZJ03fKorm/kAeYw3VL4OHKMnMeOgFtl5nQaM4jdluaRlzefofo/za6f1cNWKsyeFpaUcnrbIuVSN3mPvycuL2l/tlhA8GhLPNoQ5lo3E9Z1FU0/XglGUxnVcc6oYQfBpYT9qA+wRovkmSDl/0wPa87Oe4izYCIoyqc5xCrw9+6hHhABx8A+angQsBZxqdyzzl+0uj4H8Om6BZWrZlWOZiKER0d7Nbxw278cbruTHE3XLQeNP1Qn65FzvGM+KK91Pglui2gnzFhF9hYFzLH8o/ld8WLBtlX nameko816.07@gmail.com
